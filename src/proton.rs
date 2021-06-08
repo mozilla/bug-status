2afb926b4bd156723ff49b6553c7f3243d9369a8
Blake Winton <bwinton@latte.ca>
2021-06-08 08:45:04 -0400

    Calculate stuff we need to change, and display it.

--- src/proton.rs
+++ src/proton.rs
@@ -21,11 +21,14 @@ lazy_static! {
         password.unwrap()
     };
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct JiraIssue {
+    /** The JIRA key, FIDEFE-123 */
     key: String,
+    /** The link to this issue in JIRA */
     id: String,
     status: String,
+    points: Option<u64>,
 }
 
 impl JiraIssue {
@@ -44,7 +47,6 @@ impl JiraIssue {
             .unwrap_or_else(|| panic!("Could not get self from {:?}", item))
             .to_string();
 
-        // let item: HashMap<String, Value> = get_link(&format!("{}?fields=status", id)).unwrap();
         let fields = item
             .get("fields")
             .unwrap_or_else(|| panic!("Could not get fields from {}", &key))
@@ -62,27 +64,38 @@ impl JiraIssue {
             .unwrap_or_else(|| panic!("Could not get status name from {}", &key))
             .to_string();
 
-        Self { key, id, status }
+        let points = fields
+            .get("customfield_10014")
+            .unwrap_or(&Value::Null)
+            .as_f64()
+            .map(|x| x as u64);
+
+        Self {
+            key,
+            id,
+            status,
+            points,
+        }
     }
 }
 
-#[derive(Clone)]
-struct BugzillaBug {
-    link: String,
-    issue: JiraIssue,
+#[derive(Clone, Debug)]
+struct BugzillaJiraLink {
+    bugzilla: String,
+    jira: JiraIssue,
     cached: bool,
 }
 
-impl BugzillaBug {
-    pub fn new(issue: JiraIssue, cached_data: &Map<String, Value>) -> Self {
-        let (link, cached) = if let Some(data) = cached_data.get(&issue.key) {
+impl BugzillaJiraLink {
+    pub fn new(jira: JiraIssue, cached_data: &Map<String, Value>) -> Self {
+        let (bugzilla, cached) = if let Some(data) = cached_data.get(&jira.key) {
             (data.as_str().unwrap().to_owned(), false)
         } else {
             let link = format!(
                 "https://jira.mozilla.com/rest/api/2/issue/{}/remotelink",
-                &issue.key
+                &jira.key
             );
-            let resp: Vec<HashMap<String, Value>> = get_link(&link).unwrap();
+            let resp: Vec<HashMap<String, Value>> = get_link(&link, true).unwrap();
             let data = resp[0]["object"]
                 .as_object()
                 .unwrap_or_else(|| panic!("Could not get object from {}", link));
@@ -91,14 +104,91 @@ impl BugzillaBug {
                 data["url"]
                     .as_str()
                     .unwrap_or_else(|| panic!("Could not get url from {}", link))
-                    .to_owned(),
+                    .replace("https://bugzilla.mozilla.org/show_bug.cgi?id=", ""),
                 true,
             )
         };
         Self {
-            link,
-            issue,
+            bugzilla,
+            jira,
             cached,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct BugzillaBug {
+    id: String,
+    status: String,
+    points: Option<u64>,
+    has_patch: bool,
+    jira: JiraIssue,
+}
+
+impl BugzillaBug {
+    pub fn new(link: BugzillaJiraLink, bz_statuses: &HashMap<String, Map<String, Value>>) -> Self {
+        let id = link.bugzilla;
+        let bz_data = bz_statuses.get(&id).unwrap();
+        let status = bz_data
+            .get("status")
+            .unwrap_or_else(|| panic!("Could not get status from {:?}", bz_data))
+            .as_str()
+            .unwrap_or_else(|| panic!("Could not get status from {:?}", bz_data))
+            .to_string();
+        let points = bz_data
+            .get("cf_fx_points")
+            .unwrap_or_else(|| panic!("Could not get points from {:?}", bz_data))
+            .as_str()
+            .unwrap_or_else(|| panic!("Could not get points from {:?}", bz_data))
+            .parse::<u64>()
+            .ok();
+
+        let has_patch = if let Some(attachments) = bz_data.get("attachments") {
+            if let Some(attachments) = attachments.as_array() {
+                attachments.iter().any(|attachment| {
+                    if let Some(attachment) = attachment.as_object() {
+                        if let Some(content_type) = attachment.get("content_type") {
+                            if let Some(content_type) = content_type.as_str() {
+                                content_type == "text/x-phabricator-request"
+                            } else {
+                                false
+                            }
+                        } else {
+                            false
+                        }
+                    } else {
+                        false
+                    }
+                })
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        let jira = link.jira;
+        Self {
+            id,
+            status,
+            points,
+            has_patch,
+            jira,
+        }
+    }
+
+    pub fn get_jira_status(&self) -> String {
+        match self.status.as_str() {
+            "ASSIGNED" => {
+                if self.has_patch {
+                    "In Review".to_string()
+                } else {
+                    "In Progress".to_string()
+                }
+            }
+            "NEW" | "UNCONFIRMED" | "REOPENED" => "Open".to_string(),
+            "???" => "Unknown".to_string(),
+            _ => self.status.clone(),
         }
     }
 }
@@ -127,32 +217,55 @@ fn main() -> Result<()> {
     let cache_file = File::create(&cache_name)?;
     to_writer_pretty(cache_file, &cached_data)?;
 
-    println!(
-        "Priority:\n  \"bug_id\": \"{}\",",
-        bugs.iter()
-            .map(|item| { item.link.as_str() })
-            .collect::<Vec<_>>()
-            .join(",")
-    );
-    println!(
-        "Open:\nhttps://bugzilla.mozilla.org/buglist.cgi?bug_id={}",
-        bugs.iter()
-            .filter_map(|item| {
-                if item.issue.status != "Closed" {
-                    Some(item.link.as_str())
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>()
-            .join(",")
-    );
+    // println!(
+    //     "Open:\nhttps://bugzilla.mozilla.org/buglist.cgi?bug_id={}",
+    //     bugs.iter()
+    //         .filter_map(|item| {
+    //             if item.jira.status != "Closed" {
+    //                 Some(item.id.as_str())
+    //             } else {
+    //                 None
+    //             }
+    //         })
+    //         .collect::<Vec<_>>()
+    //         .join(",")
+    // );
+
+    let mut header1 = false;
+    for bug in &bugs {
+        if bug.points.is_some() && bug.points != bug.jira.points {
+            if !header1 {
+                println!("\n\nChanged points:");
+                header1 = true;
+            }
+            println!("  https://bugzilla.mozilla.org/show_bug.cgi?id={} ({:?}) => https://jira.mozilla.com/browse/{} ({:?})",
+                bug.id, bug.points, bug.jira.key, bug.jira.points);
+        }
+    }
+
+    let mut header2 = false;
+    for bug in &bugs {
+        if bug.get_jira_status() != bug.jira.status {
+            if !header2 {
+                println!("\n\nChanged status:");
+                header2 = true;
+            }
+            println!("  https://bugzilla.mozilla.org/show_bug.cgi?id={} ({:?}) => https://jira.mozilla.com/browse/{} ({:?})",
+                bug.id, bug.get_jira_status(), bug.jira.key, bug.jira.status);
+        }
+    }
+
+    if !header1 && !header2 {
+        println!("\n\nNo changes necessary! 🎉\n");
+    }
     Ok(())
 }
 
-fn get_link<T: for<'de> Deserialize<'de>>(link: &str) -> Result<T> {
+fn get_link<T: for<'de> Deserialize<'de>>(link: &str, auth: bool) -> Result<T> {
     let mut request = reqwest::blocking::Client::new().get(link);
-    request = request.basic_auth("bwinton@mozilla.com", Some(JIRA_PASSWORD.to_string()));
+    if auth {
+        request = request.basic_auth("bwinton@mozilla.com", Some(JIRA_PASSWORD.to_string()));
+    }
     request = request.header(reqwest::header::CONTENT_TYPE, "application/json");
     let resp = request
         .send()
@@ -167,51 +280,80 @@ fn get_link<T: for<'de> Deserialize<'de>>(link: &str) -> Result<T> {
 }
 
 fn get_bugs(
-    bugs: Vec<JiraIssue>,
+    issues: Vec<JiraIssue>,
     cached_data: &mut Map<String, Value>,
 ) -> Result<Vec<BugzillaBug>> {
-    let bar = ProgressBar::new(bugs.len() as u64);
+    let bar = ProgressBar::new(issues.len() as u64);
     bar.set_style(ProgressStyle::default_bar().template(
-        "{spinner:.green} [{elapsed_precise}] [{bar:50.cyan/blue}] ({pos}/{len}, ETA {eta})",
+        "Getting links: {spinner:.green} [{elapsed_precise}] [{bar:50.cyan/blue}] ({pos}/{len}, ETA {eta})",
     ));
 
-    let bugs: Vec<BugzillaBug> = bugs
+    let links: Vec<BugzillaJiraLink> = issues
         .into_par_iter()
         .map(|issue| {
             bar.inc(1);
-            BugzillaBug::new(issue, &cached_data)
+            BugzillaJiraLink::new(issue, &cached_data)
         })
         .collect();
     bar.finish();
 
-    let bugs = bugs
+    let mut bz_statuses = HashMap::new();
+    let bar = ProgressBar::new(links.len() as u64);
+    bar.set_style(ProgressStyle::default_bar().template(
+        "Getting bugs: {spinner:.green} [{elapsed_precise}] [{bar:50.cyan/blue}] ({pos}/{len}, ETA {eta})",
+    ));
+    for links in links.chunks(200) {
+        let list = format!("https://bugzilla.mozilla.org/rest/bug?id={}&include_fields=id,summary,status,product,priority,attachments.content_type,cf_fx_points",
+            links.iter().map(|x| x.bugzilla.as_str()).collect::<Vec<_>>().join(","));
+        let bugs: HashMap<String, Value> = get_link(&list, true).unwrap();
+        bar.inc(links.len() as u64);
+        let bugs = bugs
+            .get("bugs")
+            .unwrap_or_else(|| panic!("Could not get bugs from {}", list))
+            .as_array()
+            .unwrap_or_else(|| panic!("Could not get bugs from {}", list));
+        for bug in bugs {
+            let bug = bug
+                .as_object()
+                .unwrap_or_else(|| panic!("Could not get bug from {}", &bug));
+            let id = bug
+                .get("id")
+                .unwrap_or_else(|| panic!("Could not get id from {:?}", bug))
+                .as_u64()
+                .unwrap_or_else(|| panic!("Could not get id from {:?}", bug));
+            bz_statuses.insert(id.to_string(), bug.clone());
+        }
+    }
+    bar.finish();
+
+    let bugs = links
         .into_iter()
-        .filter_map(|mut bug| {
-            if !bug.cached {
-                let key = bug.issue.key.clone();
-                let value = Value::String(bug.link.clone());
+        .filter_map(|link| {
+            if !link.cached {
+                let key = link.jira.key.clone();
+                let value = Value::String(link.bugzilla.clone());
                 cached_data.insert(key, value);
             }
 
-            if bug.link.is_empty() {
+            if link.bugzilla.is_empty() {
                 None
             } else {
-                bug.link = bug
-                    .link
-                    .replace("https://bugzilla.mozilla.org/show_bug.cgi?id=", "");
-                Some(bug)
+                Some(BugzillaBug::new(link, &bz_statuses))
             }
         })
         .collect::<Vec<_>>();
+    // println!("bugs: {:#?}", &bugs[..10]);
     Ok(bugs)
 }
 
 fn get_list() -> Result<Vec<JiraIssue>> {
     // Get the list of issues first.
     let list =
-        // "https://jira.mozilla.com/rest/api/2/search?jql=sprint%3D1071&fields=none&maxResults=100";
-        "https://jira.mozilla.com/rest/agile/1.0/board/694/backlog?fields=none&maxResults=1000";
-    let issues: HashMap<String, Value> = get_link(&list).unwrap();
+        // Issues in a sprint: "https://jira.mozilla.com/rest/api/2/search?jql=sprint%3D1071&fields=none&maxResults=100";
+        // Issues in the backlog: "https://jira.mozilla.com/rest/agile/1.0/board/694/backlog?fields=none&maxResults=1000";
+        // All open issues:
+        "https://jira.mozilla.com/rest/agile/1.0/board/694/issue?fields=none&maxResults=1000&jql=statusCategory%20!%3D%20Done";
+    let issues: HashMap<String, Value> = get_link(&list, true).unwrap();
     let issues = issues
         .get("issues")
         .unwrap_or_else(|| panic!("Could not get issues from {}", list))
@@ -231,19 +373,20 @@ fn get_list() -> Result<Vec<JiraIssue>> {
     let mut rv = vec![];
     let bar = ProgressBar::new(issues.len() as u64);
     bar.set_style(ProgressStyle::default_bar().template(
-        "{spinner:.green} [{elapsed_precise}] [{bar:50.cyan/blue}] ({pos}/{len}, ETA {eta})",
+        "Getting issues: {spinner:.green} [{elapsed_precise}] [{bar:50.cyan/blue}] ({pos}/{len}, ETA {eta})",
     ));
-    for issues in issues.chunks(50) {
-        let list = format!("https://jira.mozilla.com/rest/api/2/search?jql=issueKey%20in%20({})&fields=status&maxResults=1000",
+    for issues in issues.chunks(200) {
+        let list = format!("https://jira.mozilla.com/rest/api/2/search?jql=issueKey%20in%20({})&fields=status,customfield_10014&maxResults=1000",
             issues.join("%2C"));
-        let issues: HashMap<String, Value> = get_link(&list).unwrap();
+        let issues: HashMap<String, Value> = get_link(&list, true).unwrap();
+        bar.inc(issues.len() as u64);
         let issues = issues
             .get("issues")
             .unwrap_or_else(|| panic!("Could not get issues from {}", list))
             .as_array()
             .unwrap_or_else(|| panic!("Could not get issues from {}", list));
+
         for issue in issues {
-            bar.inc(1);
             rv.push(JiraIssue::new(issue));
         }
     }
